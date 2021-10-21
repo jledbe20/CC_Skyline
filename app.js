d6e0fdd97e369447e43ab9a2521671964a293da3
KyleReto <40499747+KyleReto@users.noreply.github.com>
2021-10-21 16:01:24 -0400

    Added a single comment

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const config = require('./config.js');
 var app = express();
 
 app.set('view engine', 'ejs');
+// The weird path shenanigans are to make it work on any system
+// Without this, you may get weird bugs on non-windows OSes
 app.set('views', `${__dirname}/views`);
 app.use('/assets/', express.static(path.join(__dirname, 'assets')));
 app.use('/', indexRouter);
