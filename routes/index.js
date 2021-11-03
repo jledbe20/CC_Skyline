5753e96a6504009a283118a08110ac2e7c2b18c2
XavierF02 <faulkner.x.82702@student.rccc.edu>
2021-11-03 11:47:40 -0400

    Added FAQ page

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,10 @@ const Express = require('express');
 
 let router = Express.Router();
 
+router.get('/faq', async function (req, res) {
+	res.render('faq');
+});
+
 router.get('/request', async function(req, res){
 	res.render('request');
 });
