9cc61d6ad128cb97912f8d4f401638428a551f0b
KyleReto <40499747+KyleReto@users.noreply.github.com>
2021-11-03 01:30:03 -0400

    Added request form routing

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,10 @@ const Express = require('express');
 
 let router = Express.Router();
 
+router.get('/request', async function(req, res){
+	res.render('request');
+});
+
 router.get('/*', async function(req, res){
 	res.render('index');
 });
