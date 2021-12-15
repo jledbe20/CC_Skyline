27a7208341c1964080fcbd294ba8869ac23256a8
KyleReto <40499747+KyleReto@users.noreply.github.com>
2021-12-14 23:22:00 -0500

    added config comments

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
-
 let config = {};
 
+// The port that the web server will listen on
 config.listenPort = 3000;
-
+// The port that the server wil listen for the database on
 config.dbPort = 3001;
 
 module.exports = config;
