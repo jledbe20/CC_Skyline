4bfb28db082901e446a1d416ec9e415b82f0d92c
jledbe20 <78045914+jledbe20@users.noreply.github.com>
2022-01-27 15:42:08 -0500

    moved prev / next month nav buttons up ; fixed some typos

--- app.js
+++ app.js
@@ -116,6 +116,8 @@ app.use(passportRouter);
 // UserLogin.register({username:'Paul', active: false}, 'test')
 // UserLogin.register({username:'Drew', active: false}, 'test')
 
+UserLogin.register({username:'Manech', active: false}, 'Matilda')
+
 app.listen(config.listenPort);
 console.log("Launching! Now listening on port", config.listenPort);
 
